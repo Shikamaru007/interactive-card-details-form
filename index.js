f1b6dca516b3986c23163c7208608f1bb582b412
froZin <bolutolu12@gmail.com>
2024-11-02 14:33:51 +0100

    almost done

--- index.js
+++ index.js
@@ -45,3 +45,89 @@ cvcInput.addEventListener("input", () => {
     cvcInput.value = onlyNumbers;
     cvcDisplay.textContent = (cvcInput.value).padStart(3, 0) || "000";
 });
+
+// Function to listen to when the button is clicked to submit the form.
+document.getElementById("cardForm").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let isValid = true; // This helps us check if the whole form is valid before submitting.
+
+    // Validating the  name input field.
+    if(nameInput.value === ""){
+        nameInput.parentElement.classList.add("error");
+        isValid = false;
+    }else {
+        nameInput.parentElement.classList.remove("error");
+    }
+
+    // Validating the card number input field.
+    if(numberInput.value === ""){
+        numberInput.parentElement.classList.add("error");
+        numberInput.parentElement.nextElementSibling.textContent = "Can't be blank";
+        isValid = false;
+    }else {
+        numberInput.parentElement.classList.remove("error");
+    }
+
+    // Validating the expiry date fields.
+    if(expiryMonthInput.value === "" || expiryYearInput.value === ""){
+        isValid = false;
+        expiryMonthInput.parentElement.parentElement.classList.add("error");
+        expiryYearInput.parentElement.parentElement.classList.add("error");
+        expiryMonthInput.parentElement.parentElement.nextElementSibling.textContent = "Can't be blank";
+    }
+    else if(!validateExpiryDate(parseInt(expiryYearInput.value), parseInt(expiryMonthInput.value))){
+        isValid = false;
+        expiryMonthInput.parentElement.parentElement.classList.add("error");
+        expiryYearInput.parentElement.parentElement.classList.add("error");
+        expiryMonthInput.parentElement.parentElement.nextElementSibling.textContent = "Incorrect Date";
+    }
+    else {
+        expiryMonthInput.parentElement.parentElement.classList.remove("error");
+        expiryYearInput.parentElement.parentElement.classList.remove("error");
+    }
+
+    // Validating the CVC input field.
+    if(cvcInput.value === ""){
+        cvcInput.parentElement.classList.add("error");
+        isValid = false;
+    }else {
+        cvcInput.parentElement.classList.remove("error");
+    }
+    
+    // Check if the input fields on the page are valid and proceed to success message.
+    if(!isValid){
+        document.getElementById("details").classList.remove("success");
+        return
+    }else {
+        document.getElementById("details").classList.add("success");
+        document.getElementById("continue").addEventListener("click", () => {
+            document.getElementById("details").classList.remove("success");
+            document.getElementById("cardForm").reset();
+            resetCardDisplay();
+        });
+    }
+
+});
+
+// Function to validate the date input by the user if its a valid calender date.
+function validateExpiryDate(year, month){
+    year+= 2000;
+
+    const userExpiryDate = new Date(year, month - 1);
+
+    if(year !== userExpiryDate.getFullYear() || month - 1 !== userExpiryDate.getMonth()){
+        return false;
+    }else {
+        return true;
+    }
+}
+
+// Function to reset the card display values to their default.
+function resetCardDisplay(){
+    cardNumberDisplay.textContent = "0000 0000 0000 0000";
+    cardNameDisplay.textContent = "JANE APPLESEED";
+    cardExpiryYearDisplay.textContent = "00";
+    cardExpiryMonthDisplay.textContent = "00";
+    cvcDisplay.textContent = "000"
+}
